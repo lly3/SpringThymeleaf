c25e754be367a07361bfce63bc3bea5a330a6c3d
melon <momobp114@gmail.com>
2022-09-12 16:11:12 +0700

    Lab 10 Validation and Connect to API

--- src/main/java/th/ac/ku/restaurant/model/User.java
+++ src/main/java/th/ac/ku/restaurant/model/User.java
@@ -1,60 +1,25 @@
 package th.ac.ku.restaurant.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class User {
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
     @Id
     @GeneratedValue
     private UUID id;
-
     private String username;
     private String password;
     private String firstName;
     private String lastName;
     private String role;
+    private String email;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getRole() {
-        return role;
-    }
 }
